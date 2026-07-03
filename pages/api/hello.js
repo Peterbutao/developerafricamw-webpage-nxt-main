482d2f6a45d5c4cb65510b7cb302a171b7d48bb8
Peter_Butao <peterethanbutao@gmail.com>
2026-07-03 14:48:55 +0200

    runtime fix 4

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export const config = {
+  runtime: 'edge',
+}
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler() {
+  return new Response(JSON.stringify({ name: 'John Doe' }), {
+    status: 200,
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
 }
